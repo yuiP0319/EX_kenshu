e59810437f269302097c6951c6b3b39e4d0051de
Q池袋 <yui.matsumoto2@litalico.co.jp>
2020-03-31 15:31:34 +0900

    追加しました

---
+++ taiken_text/Assets/Camera_move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_move : MonoBehaviour
+{
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posx = player.transform.position.x;
+        float posy = player.transform.position.y;
+        float posz = player.transform.position.z;
+
+        transform.position = new Vector3(posx, posy + 10, posz - 10);
+    }
+}
